22476b7b6dac805639528f539ad9b50cc99deb9b
prkewlan <prkewlan@PKEWLANI>
2023-08-01 21:19:15 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ARMTemplate.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
